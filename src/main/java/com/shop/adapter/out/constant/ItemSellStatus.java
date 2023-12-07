e1f40e725f9afdb23ba0b456b9ba479eb77bc916
kij <bdgom73@naver.com>
2023-12-07 12:12:09 +0900

    ADD : Entity 추가

---
+++ src/main/java/com/shop/adapter/out/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.shop.adapter.out.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/shop/adapter/out/persistence/Member.java
@@ -0,0 +1,33 @@
+package com.shop.adapter.out.persistence;
+
+import com.shop.adapter.out.constant.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Table(name="member")
+@Getter @Setter
+@ToString
+public class Member extends BaseEntity {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
