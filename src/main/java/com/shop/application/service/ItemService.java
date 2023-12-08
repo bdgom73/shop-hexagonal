f42ea3889b3c1c24a6c3bb0bed1a047a586d66a1
kij <bdgom73@naver.com>
2023-12-08 18:22:55 +0900

    ADD : 루트 페이지 작업

--- src/main/java/com/shop/application/service/ItemService.java
+++ src/main/java/com/shop/application/service/ItemService.java
@@ -1,25 +1,32 @@
 package com.shop.application.service;
 
+import com.shop.adapter.out.persistence.repository.dto.MainItemDto;
+import com.shop.application.dto.HomeItemDto;
+import com.shop.application.dto.request.ItemSearchRequest;
 import com.shop.application.event.SavedItemEvent;
+import com.shop.application.port.in.item.GetItemUseCase;
 import com.shop.application.port.in.item.SaveItemCommand;
 import com.shop.application.port.in.item.SaveItemUseCase;
 import com.shop.application.port.out.item.CommandItemPort;
+import com.shop.application.port.out.item.LoadItemPort;
 import com.shop.domain.Item;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class ItemService implements SaveItemUseCase {
+public class ItemService implements
+        SaveItemUseCase,
+        GetItemUseCase
+{
 
     private final CommandItemPort commandItemPort;
     private final ApplicationEventPublisher publisher;
