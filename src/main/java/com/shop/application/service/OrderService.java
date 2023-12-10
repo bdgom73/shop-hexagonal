17fbfeef6f828d740e9b81aab648de7f38b0530a
kij <bdgom73@naver.com>
2023-12-10 18:04:01 +0900

    ADD : Redisson 을 사용한 주문 동시성 관련 작업

--- src/main/java/com/shop/application/service/OrderService.java
+++ src/main/java/com/shop/application/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.shop.application.service;
 
+import com.shop.application.aop.RedissonLock;
 import com.shop.application.dto.OrderHistDto;
 import com.shop.application.dto.OrderItemDto;
 import com.shop.application.dto.request.ItemRequest;
@@ -27,7 +28,6 @@ import org.springframework.util.ObjectUtils;
 import org.thymeleaf.util.StringUtils;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -49,43 +49,23 @@ public class OrderService
 
     @Override
     @Transactional
+    @RedissonLock(key = "orders")
     public Long order(String email, ItemRequest request) {
-        Item item = loadItemPort.loadById(request.getItemId())
-                .orElseThrow(EntityNotFoundException::new);
         Member member = loadMemberPort.loadByEmail(email)
                 .orElseThrow(EntityNotFoundException::new);
-        OrderItem orderItem = OrderItem.createOrderItem(item, request.getCount());
-        commandItemPort.updateStock(item);
-        Order order = Order.createOrder(member, Collections.singletonList(orderItem));
+        List<OrderItem> orderItems = addOrderItems(List.of(request));
+        Order order = Order.createOrder(member, orderItems);
         return commandOrderPort.save(order).getId();
     }
 
     @Override
     @Transactional
+    @RedissonLock(key = "orders")
     public void orders(String email, List<ItemRequest> requests) {
         Member member = loadMemberPort.loadByEmail(email)
                 .orElseThrow(EntityNotFoundException::new);
 
-        List<Long> itemIds = requests.stream().map(ItemRequest::getItemId).toList();
-        Map<Long, Item> itemMap = loadItemPort.loadAllByIn(itemIds).stream()
-                .collect(Collectors.toMap(
-                        Item::getId,
-                        _this -> _this
-                ));
-
-        List<OrderItem> orderItemList = new ArrayList<>();
-        for (ItemRequest request : requests) {
-            Item item = itemMap.get(request.getItemId());
-
-            if (ObjectUtils.isEmpty(item)) {
-                throw new EntityNotFoundException();
-            }
-
-            OrderItem orderItem = OrderItem.createOrderItem(item, request.getCount());
-            commandItemPort.updateStock(item);
-            orderItemList.add(orderItem);
-        }
-
+        List<OrderItem> orderItemList = addOrderItems(requests);
         Order order = Order.createOrder(member, orderItemList);
         commandOrderPort.save(order);
     }
@@ -139,8 +119,49 @@ public class OrderService
 
     @Override
     @Transactional
+    @RedissonLock(key = "orders")
     public void cancelOrder(Long orderId){
         commandOrderPort.cancel(orderId);
     }
 
+    /**
+     * 주문 상품 추가
+     * @param requests 상품 정보 ( 상품 식별자, 구매 수량 이 포함된 ) 객체
+     * @return List<OrderItem> 주문 상품 ( 원본 상품 도메인 이 포함된 ) 목록 반환
+     * @throws EntityNotFoundException 원본 상품이 존재 하지 않는 경우 발생 하는 예외
+     * */
+    private List<OrderItem> addOrderItems(List<ItemRequest> requests) {
+        List<Long> itemIds = requests.stream().map(ItemRequest::getItemId).toList();
+
+        Map<Long, Item> itemMap = loadItemPort.loadAllByIn(itemIds).stream()
+                .collect(Collectors.toMap(
+                        Item::getId,
+                        _this -> _this
+                ));
+
+        List<OrderItem> orderItemList = new ArrayList<>();
+        for (ItemRequest request : requests) {
+            Item item = itemMap.get(request.getItemId());
+
+            if (ObjectUtils.isEmpty(item)) {
+                throw new EntityNotFoundException();
+            }
+
+            OrderItem orderItem = OrderItem.createOrderItem(item, request.getCount());
+
+            // 상품 수량 변경
+            updateStock(item);
+
+            orderItemList.add(orderItem);
+        }
+        return orderItemList;
+    }
+
+    /**
+     * 재고 수량 변경
+     * @param item 상품 도메인 객체 ( 이미 수량 변경이 적용된 Item 객체 )
+     * */
+    private void updateStock(Item item) {
+        commandItemPort.updateStock(item);
+    }
 }
